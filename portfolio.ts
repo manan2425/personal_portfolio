ed1b026909829b54641a8ccb818bf6d43a22e000
Manan Patel <mananpatel448@gmail.com>
2025-12-01 23:24:54 +0530

    Initial commit

---
+++ portfolio.ts
@@ -0,0 +1,218 @@
+import emoji from "react-easy-emoji";
+import {
+  EducationType,
+  ExperienceType,
+  FeedbackType,
+  ProjectType,
+  SkillsSectionType,
+  SkillBarsType,
+  SEODataType,
+  SocialLinksType,
+  GreetingsType,
+} from "./types/sections";
+
+export const greetings: GreetingsType = {
+  name: "Manan Patel",
+  title: "Hi all, I'm Manan",
+  description:
+    "I'm a passionate Full Stack web developer with experience developing Full Stack web applications with Python, Django, React.js, and Cloud Technologies. I am strongly interested in learning new technologies and implementing them in my projects. I'm a self-motivated and hardworking individual who is always ready to learn new things and work in a team.",
+  resumeLink: "https://drive.google.com/file/d/1yVFlRWeh2wrpJjurMzpvhj6CXFfcOVtE/view?usp=sharing",
+};
+
+export const openSource = {
+  githubUserName: "1hanzla100",
+};
+
+export const contact = {};
+
+export const socialLinks: SocialLinksType = {
+  email: "mailto:mananpatel448@gmail.com",
+  linkedin: "https://www.linkedin.com/in/manan2425/",
+  github: "https://github.com/manan2425",
+  instagram: "https://www.instagram.com/manan_2425",
+  // facebook: 'https://www.facebook.com/1hanzla100',
+  // twitter: 'https://twitter.com/1hanzla100',
+};
+
+export const skillsSection: SkillsSectionType = {
+  title: "What I do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  data: [
+    {
+      title: "Full Stack Development",
+      lottieAnimationFile: "/lottie/skills/fullstack.json", // Path of Lottie Animation JSON File
+      skills: [
+        emoji("⚡ Building responsive Single-Page-Apps (SPA) & PWA in React.js"),
+        emoji("⚡ Building responsive static websites using Next.js"),
+        emoji("⚡ Building RESTful APIs in Django & Django REST Framework"),
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML",
+          iconifyTag: "logos:html-5",
+        },
+        {
+          skillName: "CSS",
+          iconifyTag: "logos:css-3",
+        },
+        {
+          skillName: "JavaScript",
+          iconifyTag: "logos:javascript",
+        },
+        {
+          skillName: "Reactjs",
+          iconifyTag: "vscode-icons:file-type-reactjs",
+        },
+        {
+          skillName: "Nextjs",
+          iconifyTag: "vscode-icons:file-type-light-next",
+        },
+        {
+          skillName: "Nodejs",
+          iconifyTag: "logos:nodejs-icon",
+        },
+        {
+          skillName: "PHP",
+          iconifyTag: "logos:php",
+        },
+        {
+          skillName: "MongoDB",
+          iconifyTag: "logos:mongodb-icon",
+        },
+        {
+          skillName: "Python",
+          iconifyTag: "logos:python",
+        },
+        {
+          skillName: "Github",
+          iconifyTag: "akar-icons:github-fill",
+        },
+      ],
+    },
+  ],
+};
+
+export const SkillBars: SkillBarsType[] = [
+  {
+    Stack: "Frontend/Design", //Insert stack or technology you have experience in
+    progressPercentage: "95", //Insert relative proficiency in percentage
+  },
+  {
+    Stack: "Backend",
+    progressPercentage: "90",
+  },
+  {
+    Stack: "Programming",
+    progressPercentage: "95",
+  },
+];
+
+export const educationInfo: EducationType[] = [
+  {
+    schoolName: "Birla Vishvakarma Mahavidhaylaya Engineering College",
+    subHeader: "Bachelor of Technology in Computer Engineering",
+    duration: "September 2022 - May 2026",
+    desc: "",
+    //grade: "Grade A",
+    descBullets: [], // Array of Strings
+  },
+];
+
+export const experience: ExperienceType[] = [
+  {
+    role: "Project Intern",
+    company: "Helius Wellness",
+    companyLogo: "/img/icons/common/helius-wellness.jpeg",
+    date: "May 2025 - June 2025",
+    desc: "Crafted robust mobile application backends employing Django, Python, and REST APIs. Leveraged Django REST Framework, PostgreSQL, AWS, Firebase, Stripe, and WebSocket for efficient development. Collaborated across teams to surpass client expectations. Designed intuitive UIs, empowering clients to effortlessly manage their applications with a visually captivating interface.",
+    // descBullets: [
+    // 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
+    // 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    // ],
+  }, {
+    role: "AI - ML Summer Intern",
+    company: "Ural Federal University, Russia",
+    companyLogo: "/img/icons/common/urfu.png",
+    date: "July 2025 - Aug 2025",
+    desc: "During my internship at Ural Federal University (UrFU), I worked on hands-on research and development tasks that strengthened both my technical and analytical skills. I collaborated with international peers and faculty, gaining exposure to advanced methodologies and a global work culture. My work involved solving real-world problems using modern technologies, writing clean and structured code, and presenting findings effectively. This experience enhanced my ability to work in diverse teams, improved my problem-solving approach, and helped me grow as a developer with a broader global perspective.",
+  },
+  {
+    role: "Summer Intern",
+    company: "Tech Elecon Pvt. Ltd.",
+    companyLogo: "/img/icons/common/tech_elecon.png",
+    date: "May 2025 - June 2025",
+    desc: "Crafted robust mobile application backends employing Django, Python, and REST APIs. Leveraged Django REST Framework, PostgreSQL, AWS, Firebase, Stripe, and WebSocket for efficient development. Collaborated across teams to surpass client expectations. Designed intuitive UIs, empowering clients to effortlessly manage their applications with a visually captivating interface.",
+    // descBullets: [
+    // 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
+    // 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    // ],
+  },
+  {
+    role: "Web Developer Intern",
+    company: "Ultron Technologies",
+    companyLogo: "/img/icons/common/ultron.png",
+    date: "Aug 2022 - Jun 2023",
+    desc: "Crafting diverse web applications, I've elevated functionality and user engagement. Spearheaded Visual Portfolio, leveraging AI for captivating presentations from parsed text. Proficiently set up and optimized applications on Linux servers, guaranteeing smooth scalability and top-notch performance for seamless user experiences.",
+  },
+];
+
+export const projects: ProjectType[] = [
+  {
+    name: "developer-portfolio",
+    desc: "Software Developer Portfolio Template built with react.js and next.js bootstrap that helps you showcase your work and skills as a software developer.",
+    github: "https://github.com/1hanzla100/developer-portfolio",
+    link: "https://developer-portfolio-1hanzla100.vercel.app/",
+  },
+  {
+    name: "Giebo",
+    desc: "A Podcast Platform where creators can easily publish and sell their podcasts, and users can purchase with Handcash BitcoinSV, the future of fast and secure transactions.",
+    link: "https://gibeo.io/",
+  },
+  {
+    name: "O Mejor Oferta",
+    desc: "O Mejor Oferta is a mobile marketplace for buying and selling items nearby, helping users find deals on things they want and make money from items they no longer need.",
+    link: "https://play.google.com/store/apps/details?id=com.duseca.mejor_oferta",
+  },
+  {
+    name: "Hooligan Culture",
+    desc: "A Ecommerce Platform, where shopping meets the future! With HandCash BitcoinSV integration, you can authenticate and purchase products with ease and confidence.",
+    link: "https://hooliganculture.com/",
+  },
+];
+
+export const feedbacks: FeedbackType[] = [
+  {
+    name: "Syed Jamal",
+    role: "Frontend Developer at Meganos Software",
+    feedback:
+      "You were a wonderful addition to our team, alhumdulillah. I'm quite impressed with your abilities and professional practices. You approach problems from a unique perspective and always ask the right question. Finding a good teammate like Hanzla is extremely difficult. He has contributed to the growth of our organization and continues to do so. May Allah bless you with everything you desire.",
+  },
+  {
+    name: "Wajahat Malek",
+    role: "CEO at Duseca Software",
+    feedback:
+      "Hanzla has a deep understanding of web development technologies and is adept at using them to build scalable, robust, and secure web applications. He has worked on various projects, ranging from small-scale to large-scale, and has shown his ability to work collaboratively with others while maintaining a high level of productivity and quality. Hanzla is a pleasure to work with, and I have no doubt that he will be an asset to any team or organization. I highly recommend Hanzla for any Full Stack Django, React.js, or Blockchain development projects, and I wish him all the best in his future endeavors.",
+  },
+  {
+    name: "Zaid Zaffar",
+    role: "CEO at ZR Technologies",
+    feedback:
+      "Very skilled at what he does and has an in depth knowledge of Django and he's always ready to handle any challenges thrown at him. He's been a valuable member of my team and i look forward to working with him in the years to come",
+  },
+];
+
+// See object prototype on /types/section.ts page
+export const seoData: SEODataType = {
+  title: "Manan Patel",
+  description: greetings.description,
+  author: "Manan Patel",
+  image: "https://avatars.githubusercontent.com/u/59178380?v=4",
+  url: "https://developer-portfolio-1hanzla100.vercel.app",
+  keywords: [
+    "Manan",
+    "Manan Patel",
+    "Portfolio",
+    "Manan Portfolio ",
+    "Manan Patel Portfolio",
+  ],
+};
